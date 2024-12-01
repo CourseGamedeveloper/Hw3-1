35118067604bf327a8925e95600093b17bb76cb0
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-12-01 21:27:15 +0200

    Update TimedSpawner.cs

--- Assets/Scripts/2-spawners/TimedSpawner.cs
+++ Assets/Scripts/2-spawners/TimedSpawner.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 /**
  * This component spawns the given object at fixed time-intervals at its object position.
  */
-public class TimedSpawner: MonoBehaviour {
+public class TimedSpawner : MonoBehaviour {
     [SerializeField] Mover prefabToSpawn;
     [SerializeField] Vector3 velocityOfSpawnedObject;
     [SerializeField] float secondsBetweenSpawns = 1f;
 
     // OLD CODE using coroutines:
-    //
-
-    //void Start() {
-    //    this.StartCoroutine(SpawnRoutine());
-    //    Debug.Log("Start finished");
-    //}
-
-    //IEnumerator SpawnRoutine() {
-    //    while (true) {
-    //        GameObject newObject = Instantiate(prefabToSpawn.gameObject, transform.position, Quaternion.identity);
-    //        newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
-    //        yield return new WaitForSeconds(secondsBetweenSpawns);
-    //    }
-    //}
+    // void Start() {
+    //     this.StartCoroutine(SpawnRoutine());
+    //     Debug.Log("Start finished");
+    // }
+    // IEnumerator SpawnRoutine() {
+    //     while (true) {
+    //         GameObject newObject = Instantiate(prefabToSpawn.gameObject, transform.position, Quaternion.identity);
+    //         newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
+    //         yield return new WaitForSeconds(secondsBetweenSpawns);
+    //     }
+    // }
 
     // NEW CODE using async-await and Awaitable - works on Windows but not on WebGL
-    //
     void Start() {
         SpawnRoutine();
         Debug.Log("Start finished");
@@ -41,5 +37,4 @@ public class TimedSpawner: MonoBehaviour {
             // See here for more options: https://docs.unity3d.com/6000.0/Documentation/Manual/async-awaitable-introduction.html
         }
     }
-
 }
