8ed948f54a2ecd25b9e74f56620b44041c3ee587
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-12-01 21:24:36 +0200

    Update InputMover.cs

--- Assets/Scripts/1-movers/InputMover.cs
+++ Assets/Scripts/1-movers/InputMover.cs
@@ -1,21 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 /**
  * This component moves its object when the player clicks the arrow keys.
  */
-public class InputMover: MonoBehaviour {
+public class InputMover : MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 10f;
-
     [SerializeField] InputAction move = new InputAction(
         type: InputActionType.Value, expectedControlType: nameof(Vector2));
 
-    void OnEnable()  {
+    void OnEnable() {
         move.Enable();
     }
 
-    void OnDisable()  {
+    void OnDisable() {
         move.Disable();
     }
 
@@ -23,7 +22,7 @@ public class InputMover: MonoBehaviour {
         Vector2 moveDirection = move.ReadValue<Vector2>();
         Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
         transform.position += movementVector;
-        //transform.Translate(movementVector);
+        // transform.Translate(movementVector);
         // NOTE: "Translate(movementVector)" uses relative coordinates - 
         //       it moves the object in the coordinate system of the object itself.
         // In contrast, "transform.position += movementVector" would use absolute coordinates -
